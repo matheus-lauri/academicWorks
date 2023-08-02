847efd9917a70702a7f6512fff32cc648121c25f
Matheus Lauritzen <97478772+matheus-lauri@users.noreply.github.com>
2023-08-02 15:15:30 -0300

    Add files via upload

---
+++ codes-and-projects/2semestre/algoritmos2/scripts_chalenges/scripts_chalenges/chalenge_2/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float cont= 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cont = cont + Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && (cont > 2.0f))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cont= 0.0f;
+        }
+    }
+}
